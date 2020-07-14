c722f17649dba43c7ab1ca0c37332ff4088fc618
Isra <israel_2896@hotmail.com>
2020-07-14 18:24:15 -0500

    Metodos y propiedades en JS, se agrego un DOM

---
+++ MetodsyProp.js
@@ -0,0 +1,14 @@
+var texto = "Israel Martin";
+//substring permite cortar una cadena de text
+//var text2 =  texto.substring(0, 5);
+//permite ver la posicion de una letra
+//var text2 = texto.indexOf("a", 5);
+//busca letras de izq. a dere.
+//var text2 = texto.lastIndexOf("a");
+//reemplaza una cadena de text por otra
+//var text2 = texto.replace(texto, "Ismael");
+//convierte el text a MAYUS
+var text2 = texto.toUpperCase();
+//convierte el text a MINUS
+//var text2 = texto.toLowerCase();
+document.write(text2);
---
+++ dom.js
@@ -0,0 +1,5 @@
+//los ID solo se usan en JS, cuando es en CSS se usan class
+//permite mostrar todos los elementos html mediante la etiqueta p.
+var elementoP = document.getElementsByTagName("p");
+//permite mostrar elemento html mediante un ID de la etiqueta
+var segundoP = document.getElementById("segundo");
