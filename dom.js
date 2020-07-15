eecc27b5f97e23f7f5336a7b5375e48f8a801f25
Isra <israel_2896@hotmail.com>
2020-07-15 11:38:33 -0500

    Creacion de nodos DOM

--- dom.js
+++ dom.js
@@ -3,3 +3,13 @@
 var elementoP = document.getElementsByTagName("p");
 //permite mostrar elemento html mediante un ID de la etiqueta
 var segundoP = document.getElementById("segundo");
+//crear elemento
+var element = document.createElement("h2");
+//Crear un nodo de texto
+var content =  document.createTextNode("Este es el titulo");
+//Añadir el nodo de texto al elemento
+element.appendChild(content);
+//Agregar atributo al elemento
+element.setAttribute("align", "center");
+//Agregar el elemento al docuemento
+document.getElementById("subtitulo").appendChild(element)
