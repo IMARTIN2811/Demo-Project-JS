c6335da66b0daee03bbc155bbe5b92743d6282c9
Isra <israel_2896@hotmail.com>
2020-07-16 17:10:10 -0500

    Modificacion de estilos de los elementos

---
+++ js/encabezado.js
@@ -0,0 +1,12 @@
+//accede al elemento del html
+var element = document.getElementById("encabezado");
+//si hay dos clases permite quitar uno
+element.className =  element.className.replace("tituloGrande", "");
+//accede a los estilos del elemento emcabezado
+//element.style.color = "navy";
+/**
+element.style.background = "navy";
+element.style.color = "#fff";
+element.style.paddingTop = "20px";
+element.style.fontFamily = "20px";
+ */
